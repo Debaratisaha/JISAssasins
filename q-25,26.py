a7d37cc3344241af9599a085d7e668d74f867bd2
Debaratisaha <46235871+Debaratisaha@users.noreply.github.com>
2018-12-29 14:42:20 +0530

    Add files via upload

---
+++ q-25,26.py
@@ -0,0 +1,26 @@
+'''
+q-25
+'''
+'''c2=input("enter a character ")
+
+if(c2>='a' and c2<='z') or (c2>='A' and c2<='Z'):
+    print("The given character ",c2,"is an alphabet")
+else:
+    print("The given character ",c2, "is a digit")
+
+  '''
+
+'''q-26
+'''
+'''
+salary=int(input("enter your salary"))
+if (salary>=5000):
+           hra=salary*0.15
+           da=salary*1.5
+           gross_salary=hra+da+salary
+else:
+           hra=salary*0.10
+           da=salary*1.1
+           gross_salary=hra+da+salary
+print(gross_salary)
+'''
