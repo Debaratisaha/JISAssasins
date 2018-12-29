64dcf098aef1a9cb02d5b47cef35cd56e3155937
Debaratisaha <46235871+Debaratisaha@users.noreply.github.com>
2018-12-29 12:51:16 +0530

    Add files via upload

---
+++ q 12,18,24 by debarati.py
@@ -0,0 +1,29 @@
+'''
+(18)
+'''
+x=int(input("first number"))
+y=int(input("enter the power"))
+p=1
+for i in range(y):
+    p*=x
+print(p)
+
+
+
+'''
+(12)
+'''
+
+str35="sima is very intelligent"
+list35=str35.split(" ")
+list35.reverse()
+print(' '.join(list35))
+
+
+
+
+'''(24)
+'''
+Num=10
+for i in range(1,11):
+    print(Num *i)
